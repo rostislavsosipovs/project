4224deafeef6933b2d964249a59325ce63149868
Ivan <example@gmail.com>
2020-08-09 21:54:08 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const person = 'Alex';
+const bool = true;
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+let arr = ['plum.png', 'orange.jpeg', 6, 'apple.bmp'];
+console.log(arr[0]);
+
+// const result = confirm("Are you here?");
+// const answer = prompt("Вам есть 18?", "");
+
+const answers = [];
+
+// answers[0] = prompt('Как вас зовут?', '');
+// answers[1] = prompt('Фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+console.log(typeof(answers));
